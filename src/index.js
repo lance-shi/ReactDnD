198688bf0ab676807c910fc4f784d267b8885c73
Lance Shi <yang.shi@rea-group.com>
2019-02-14 16:56:31 +1100

    React js simple drag and drop

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppDragDropDemo from './components/AppDragDropDemo'
+
+let App = document.getElementById("app");
+
+ReactDOM.render(<AppDragDropDemo name="Lance"/>, App);
